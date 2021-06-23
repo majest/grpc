ea048de7b30ac324e3f300d2cbc45082cb2c106f
NintySwinty <31569763+NintyS@users.noreply.github.com>
2021-06-24 01:20:23 +0200

    Update chat.go

--- chat/chat.go
+++ chat/chat.go
@@ -11,6 +11,6 @@ type Server struct { // Pusta struktura serwera
 }
 
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) { // Funckja która przyjmuję kontekst i wiadomość i zwraca wiadomość i error
-	log.Printf("Messages: $V", message.Body) // Wypisywanie wiadomości
+	log.Printf("Messages: %v", message.Body) // Wypisywanie wiadomości
 	return &Message{Body: "Hello from the server!"}, nil // Zwracanie wiadomości i błędu ( a raczej jego brak )
 }
