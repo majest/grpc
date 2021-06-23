494e9a7a9e650dbaf702be2ca670fc9cbce5c42d
NintySwinty <31569763+NintyS@users.noreply.github.com>
2021-06-24 00:56:06 +0200

    Typo

--- chat/chat.go
+++ chat/chat.go
@@ -12,5 +12,5 @@ type Server struct { // Pusta struktura serwera
 
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) { // Funckja która przyjmuję kontekst i wiadomość i zwraca wiadomość i error
 	log.Printf("Messages: $V", message.Body) // Wypisywanie wiadomości
-	return &Message{Body: "Hello from the server!"}, nill // Zwracanie wiadomości i błędu ( a raczej jego brak )
+	return &Message{Body: "Hello from the server!"}, nil // Zwracanie wiadomości i błędu ( a raczej jego brak )
 }
